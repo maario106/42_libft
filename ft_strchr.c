aaf8659f552281053aacab48ce6cf4505db634b0
mario <127196327+maario106@users.noreply.github.com>
2026-04-16 15:35:24 +0000

    Ejs: Segunda columna completa

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,5 +1,7 @@
 #include "libft.h"
 
+#include <stdio.h>
+
 size_t ft_strlen(const char *s)
 {
     size_t counter;
@@ -12,12 +14,39 @@ size_t ft_strlen(const char *s)
 
 char *ft_strchr(const char *s, int c)
 {
-    int i;
+    size_t i;
     size_t len;
 
     i = 0;
     len = ft_strlen(s) + 1;
     while (s[i] != c && i < len)
         i++;
-        return (s + i);
+    if (i == len && c != '\0')
+        return (NULL);
+    else
+        return ((char *)s + i);
+}
+
+int main()
+{
+    // define a string
+    const char* str = "alssdnasdbas?";
+    // define a char ch to be searched in str
+    char ch = '\0';
+
+    // Use strchr to find the first occurrence of the
+    // character 's'
+    const char* result = ft_strchr(str, ch);
+
+    if (result != NULL) {
+        // Calculate the position by subtracting the base
+        // pointer from the result pointer
+        printf("Character '%c' found at position: %ld\n",
+               ch, result - str);
+    }
+    else {
+        printf("Character '%c' not found.\n", ch);
+    }
+
+    return 0;
 }
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-size_t ft_strlen(const char *s)
+ ft_strlen(const char *s)
 {
     size_t counter;
 
