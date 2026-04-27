c5927c275f2952f41a790c41a881fb2fb16547be
mario <127196327+maario106@users.noreply.github.com>
2026-04-27 10:04:15 +0000

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,15 +1,53 @@
 # include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+size_t  ft_strlen(const char *s)
 {
+    size_t counter;
+
+    counter = 0;
+    while (s[counter] != '\0')
+        counter++;
+    return (counter);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	size;
 	char	*ptr;
 	size_t	counter;
 
-	ptr = malloc(sizeof(char) * (len + 1));
+	size = ft_strlen(s);
+	ptr = malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
 	counter = 0;
-	while (s[start] != '\0' && counter < len)
+	while (counter < size)
+	{
+		ptr[counter] = s[counter];
+		counter++;
+	}
+	ptr[counter] = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	size;
+	size_t	counter;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+		return (ft_strdup(""));
+	if (size - start < len)
+		len = size - start;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	counter = 0;
+	while (s[start + counter] != '\0' && counter < len)
 	{
 		ptr[counter] = s[start + counter];
 		counter++;
--- libft.h
+++ libft.h
@@ -7,4 +7,6 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+
 
