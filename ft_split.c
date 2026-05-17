4fc3fa8bd1721566a2c72bae6095faeff009fee1
Mario Rosas Navarro <mrosas.dev@gmail.com>
2026-05-17 17:20:33 +0200

    feat: Split conseguido

--- ft_split.c
+++ ft_split.c
@@ -3,42 +3,163 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mrosas-n <mrosas-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 17:56:43 by codespace         #+#    #+#             */
-/*   Updated: 2026/05/16 15:36:15 by codespace        ###   ########.fr       */
+/*   Updated: 2026/05/17 17:17:27 by mrosas-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count_words(char const *s, char c)
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
 {
-	int	nwords;
-	int	i;
-	
-	nwords = 0;
-	i = 0;
-	while (s[i])
+	size_t	counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+		counter++;
+	return (counter);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	size;
+	char	*ptr;
+	size_t	counter;
+
+	size = ft_strlen(s);
+	ptr = malloc(sizeof(char) * (size + 1));
+	if (ptr == NULL)
+		return (NULL);
+	counter = 0;
+	while (counter < size)
 	{
-		if (s[i - 1] == c && s[i] == c)
-			nwords++;
+		ptr[counter] = s[counter];
+		counter++;
+	}
+	ptr[counter] = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	size;
+	size_t	counter;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+		return (ft_strdup(""));
+	if (size - start < len)
+		len = size - start;
+	ptr = (char *) malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	counter = 0;
+	while (s[start + counter] != '\0' && counter < len)
+	{
+		ptr[counter] = s[start + counter];
+		counter++;
+	}
+	ptr[counter] = '\0';
+	return (ptr);
+}
+
+static void	free_split(char **ptr, int n)
+{
+	while (n--)
+		free(ptr[n]);
+	free(ptr);
+}
+
+static int	iterate_word(char const *s, char c)
+{
+	int	size;
+
+	size = 0;
+	if (!s)
+		return (-1);
+	while (*s && *s != c)
+	{
+		size++;
 		s++;
 	}
+	return (size);
+}
+
+static int	count_words(char const *s, char c)
+{
+	int	nwords;
+	int word_len;
+	
+	if (!s)
+		return (0);
+	nwords = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		word_len = iterate_word(s, c);
+		if (word_len > 0)
+		{
+			nwords++;
+			s += word_len;
+		}
+	}
+	return (nwords);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	*ptr;
-	int		n_words;
-	int		i;
+	char	**ptr;
+	int 	word_len;
+	int 	ptr_index;
 	
-	n_words = 0;
-	i = 0;
-	while (s[i])
+	if (!s)
+		return (NULL);
+	ptr = (char **) malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!ptr)
+		return (NULL);
+	ptr_index = 0;
+	while (*s)
 	{
-		if (s[i] != c)
-			
-		
+		while (*s == c)
+			s++;
+		word_len = iterate_word(s, c);
+		if (word_len > 0)
+		{
+			ptr[ptr_index] = ft_substr(s, 0, word_len);
+			if (!ptr[ptr_index++])
+				return (free_split(ptr, ptr_index - 1), NULL);
+			s += word_len;
+		}
 	}
+	ptr[ptr_index] = NULL;
+	return (ptr);
+}
+
+int main()
+{
+	char *str = "pues genial entonces qué meravilla";
+	char c = 'e';
+	char **split;
+	int i;
+
+
+	printf("[%s]\n\n", str);
+	printf("Nwords: %d\n", count_words(str, c));
+
+	split = ft_split(str, c);
+	i = 0;
+	while(split[i]){
+		printf("Palabra %d -> %s\n", i, split[i]);
+		i++;
+	}
+
+	return (0);
 }
