c6acdd1f0d54a0a00a9d25d84e43cd53c08d08d7
mario <127196327+maario106@users.noreply.github.com>
2026-05-17 19:50:00 +0000

    feat: strmapi done

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 18:14:15 by codespace         #+#    #+#             */
-/*   Updated: 2026/05/07 18:14:40 by codespace        ###   ########.fr       */
+/*   Updated: 2026/05/17 19:43:42 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,48 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	size_t			len;
+	char			*ptr;
+	unsigned int	i;
 	
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return(NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
+
+// int main(void)
+// {
+// 	char	*result;
+
+// 	// Test 1: uppercase
+// 	result = ft_strmapi("hello world", to_upper);
+// 	printf("Test 1 (uppercase): %s\n", result);
+// 	free(result);
+
+// 	// Test 2: add index (a→a, b+1→c, c+2→e, ...)
+// 	result = ft_strmapi("abcdef", add_index);
+// 	printf("Test 2 (add index): %s\n", result);
+// 	free(result);
+
+// 	// Test 3: NULL string
+// 	result = ft_strmapi(NULL, to_upper);
+// 	printf("Test 3 (NULL): %s\n", result ? result : "(null)");
+
+// 	// Test 4: empty string
+// 	result = ft_strmapi("", to_upper);
+// 	printf("Test 4 (empty): [%s]\n", result);
+// 	free(result);
+
+// 	return (0);
+// }
