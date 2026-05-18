a48bae43dffd7c5d8200a1a123a4c46f1edf4e6b
mario <127196327+maario106@users.noreply.github.com>
2026-05-18 09:23:51 +0000

    pending: lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	
-}
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+
+// }
