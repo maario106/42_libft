d9fda39c971e87763cb429b298566f97e50ade15
mario <127196327+maario106@users.noreply.github.com>
2026-05-27 09:53:39 +0000

    feat: lstmap done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 17:54:40 by codespace         #+#    #+#             */
-/*   Updated: 2026/05/18 09:15:08 by codespace        ###   ########.fr       */
+/*   Updated: 2026/05/27 09:50:27 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
